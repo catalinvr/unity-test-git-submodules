d650f9e9c7ae843cdeb58c77e64984824196168e
Lars Wobus <lars.wobus@web.de>
2018-08-19 12:32:41 +0200

    Batch file requires 2 arguments
    - submodule source and target location within
    Assets folder must be specified

--- scripting/symlink-submodule.bat
+++ scripting/symlink-submodule.bat
@@ -1,1 +1,29 @@
-mklink /J DummyProject\Assets\com.company.product_2 submodules\unity-sample-project\SampleProject\Assets\com.company.product
+REM Open cmd as administrator
+REM Example: scripting\symlink-submodule submodules\unity-sample-project\SampleProject\Assets\com.company.product DummyProject\Assets\com.company.product_2
+
+@ECHO off
+
+SET SubmoduleSource=%1
+SET SubmoduleTarget=%2
+
+IF [%SubmoduleSource%] == [] GOTO missingSource
+IF [%SubmoduleTarget%] == [] GOTO missingTarget
+
+REM mklink /J DummyProject\Assets\com.company.product_2 submodules\unity-sample-project\SampleProject\Assets\com.company.product
+mklink /J %SubmoduleTarget% %SubmoduleSource%
+
+GOTO :EOF
+
+
+
+
+@ECHO on
+
+:missingSource
+ECHO "Source undefined"
+GOTO :EOF
+
+:missingTarget
+ECHO "Target undefined"
+GOTO :EOF
+
